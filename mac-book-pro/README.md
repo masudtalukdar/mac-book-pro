a391265e83ac32f620fd78c52fcbf10ece5e51c6
masudtalukdar <masudtalukdar.cn@gmail.com>
2021-11-24 20:04:59 +0800

    added live link

---
+++ mac-book-pro/README.md
@@ -0,0 +1,3 @@
+# mac-book-pro
+
+[Click here for LIve Site Link](https://drone-shop-2f2e9.web.app/).
---
+++ mac-book-pro/js/main.js
@@ -0,0 +1,87 @@
+// change price function
+function changePrice(product, price) {
+  const productCost = document.getElementById(product + '-cost');
+  productCost.innerText = price;
+  calculateToatl();
+}
+//getting cost of the extra product
+function getValue(product) {
+  const productCost = document.getElementById(product + '-cost').innerText;
+  const productPrice = parseInt(productCost);
+  return productPrice;
+}
+//calculating total
+function calculateToatl() {
+  const memoryPrice = getValue('memory');
+  const storagePrice = getValue('storage');
+  const deliveryPrice = getValue('delivery');
+  const bestPrice = 1299;
+  // caculate total and set total in total price
+  const totalPrice = memoryPrice + storagePrice + deliveryPrice + bestPrice;
+  const total = document.getElementById('total');
+  total.innerText = totalPrice;
+  //get the last total field
+  const totalField = document.getElementById('total-field');
+  totalField.innerText = totalPrice;
+}
+
+// change price for memory
+document
+  .getElementById('memory1-option')
+  .addEventListener('click', function () {
+    changePrice('memory', 0);
+  });
+document
+  .getElementById('memory2-option')
+  .addEventListener('click', function () {
+    changePrice('memory', 180);
+  });
+// change price for storage
+document
+  .getElementById('storage1-option')
+  .addEventListener('click', function () {
+    changePrice('storage', 0);
+  });
+document
+  .getElementById('storage2-option')
+  .addEventListener('click', function () {
+    changePrice('storage', 100);
+  });
+document
+  .getElementById('storage3-option')
+  .addEventListener('click', function () {
+    changePrice('storage', 180);
+  });
+// change price for delivery
+document
+  .getElementById('delivery1-option')
+  .addEventListener('click', function () {
+    changePrice('delivery', 0);
+  });
+document
+  .getElementById('delivery2-option')
+  .addEventListener('click', function () {
+    changePrice('delivery', 20);
+  });
+
+//checking the cupon
+document.getElementById('cupon-btn').addEventListener('click', function () {
+  const cuponBtn = document.getElementById('cupon-btn');
+  const cuponInput = document.getElementById('cupon-input');
+  const cuponValue = cuponInput.value;
+  const totalField = document.getElementById('total-field');
+  const totalPrice = parseInt(totalField.innerText);
+
+  const discount = (totalPrice * 20) / 100;
+  const discPrice = totalPrice - discount;
+
+  //validating cupon
+  if (cuponValue == 'stevekaku') {
+    totalField.innerText = discPrice;
+    cuponInput.value = '';
+    cuponBtn.disabled = true;
+  } else {
+    console.log(alert('Cupon does not match !'));
+  }
+  cuponInput.value = '';
+});
